0b7a18ebf8bc62cb64aefcd9b24581e6bcb06e00
Vishal <vishalj2023@outlook.com>
2024-07-16 09:22:53 +0530

    Day 3

--- Day 3 - Control Structure/Activity5.js
+++ Day 3 - Control Structure/Activity5.js
@@ -1,9 +1,13 @@
 // Combining Condition
 // leap year 
 
-let year = 2004
+let year = 2003
 
 if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
 {
     console.log(`${year} is Leap year`);
+}
+else
+{
+    console.log(`${year} is not a Leap year`)
 }
