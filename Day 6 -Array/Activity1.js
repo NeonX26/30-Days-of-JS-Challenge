484eba1f124f025f5910558b15e795182130b33f
Vishal <vishalj2023@outlook.com>
2024-07-19 23:15:08 +0530

    .

---
+++ Day 6 -Array/Activity1.js
@@ -0,0 +1,6 @@
+// Array Creation and Access
+
+const arr = [1,2,3,4,5];
+console.log(arr)
+
+console.log(`The First Element is '${arr[0]}' and the last Element is '${arr[4]}'`);
---
+++ Day 6 -Array/Activity2.js
@@ -0,0 +1,18 @@
+// Array methods
+
+// 1. push
+const nums = [1,2,3,4,5]
+nums.push(6)
+console.log("🚀 ~ nums:", nums)
+
+// 2. pop
+nums.pop()
+console.log("🚀 ~ nums:", nums)
+
+// 3. shift
+nums.shift()  //it will remove the first element in the array
+console.log("🚀 ~ nums:", nums)
+
+// 4. unshift
+nums.unshift(0)  //it will add element at the starting of the array
+console.log("🚀 ~ nums:", nums)
