c8bc330a9defe934cfc9134a215fb697b59dab9c
Vishal <vishalj2023@outlook.com>
2024-07-21 09:48:54 +0530

    Day 6

--- Day 6 -Array/Activity5.js
+++ Day 6 -Array/Activity5.js
@@ -1,1 +1,14 @@
 // Multi-Dimentional Array
+//log entire elements in the 2D array
+let arr = [[1,2],[3,4]];
+
+for(let i = 0; i< 2; i++)
+{
+    for(let j = 0; j< 2; j++)
+    {
+        console.log(arr[i][j]);
+    }
+}
+
+// access the specific elements in the array
+console.log(arr[1][0]);
